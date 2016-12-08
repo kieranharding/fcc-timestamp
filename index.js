f873eaf0c4e12aa072dc02fab4dccc47bc8e2d41
Kieran Harding <kieranharding@gmail.com>
2016-12-08 05:51:44 -0600

    Use Express
    It probably would have been easier to start with this,
    if I had realized I wanted a landing page.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 // Deployed at https://kah-fcc-timestamp.herokuapp.com/
 
 const http = require('http')
-const url = require('url')
 const strftime = require('strftime')
+const express = require('express')
+const path = require('path')
 
 function get_unix (stamp) {
   return 1 * stamp || Date.parse(stamp.replace(new RegExp('%20', 'g'), ' ')) / 1000 || null
@@ -20,18 +21,35 @@ function get_natural (stamp) {
   } 
 }
 
-const server = http.createServer((req, res) => {
-  const path = url.parse(req.url, true).pathname.slice(1)
-  const result = {
-    'unix': get_unix(path),
-    'natural': get_natural(path)
-  }
+var app = express()
 
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  res.end(JSON.stringify(result))
+app.set('port', process.env.PORT || 5000) // This is from the heroku example, not sure if they care what port
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-server.listen(process.env.PORT || 5000)
+app.get('/:stamp', (req, res) => {
+  res.send(JSON.stringify({
+    unix: get_unix(req.params.stamp),
+    natural: get_natural(req.params.stamp)
+  }))
+})
+
+app.listen(app.get('port'))
+
+// const server = http.createServer((req, res) => {
+//   const path = url.parse(req.url, true).pathname.slice(1)
+//   const result = {
+//     'unix': get_unix(path),
+//     'natural': get_natural(path)
+//   }
+
+//   res.writeHead(200, {'Content-Type': 'application/json'})
+//   res.end(JSON.stringify(result))
+// })
+
+// server.listen(process.env.PORT || 5000)
 
 module.exports = {
   get_unix: get_unix,
